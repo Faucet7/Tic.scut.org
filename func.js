a0efcc5f8bdd8373364ebc858056d7230f317c28
goodhyz <202230444238@mail.scut.edu.cn>
2023-11-16 01:01:29 +0800

    修改按按钮没有反应的bug

---
+++ func.js
@@ -0,0 +1,27 @@
+function changecolor() {
+    var elem = document.getElementById("num2");
+    var currentColor = window.getComputedStyle(elem).color; // 获取当前颜色
+    var randomColor = getRandomColor(); // 生成一个随机颜色
+    elem.style.color = randomColor;
+  }
+  
+ function addone() {
+    var elem = document.getElementById("num1");
+    var present = parseInt(elem.value) || 0;
+    elem.value = present + 1;
+  }
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function changeSign(){
+    var elem = document.getElementById("num3");
+    var present = parseInt(elem.value) || 0;
+    elem.value =0-present;
+}
