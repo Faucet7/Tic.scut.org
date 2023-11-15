b883d3734ac4dd09d9dc2257afca048faf1ec5c7
hhhJB <guangtouchangkaishen@outlook.com>
2023-11-15 16:14:15 +0800

    改变颜色

--- test1.js
+++ test1.js
@@ -3,3 +3,12 @@ function addone(id) {
     var presnt = parseInt(elem.value) || 0;
     elem.value = presnt + 1;
 }
+function changecolor() {
+    let button = document.getElementById("num2"); // access the button by id
+    let color = button.style.color;
+    if (color == "red") { // if button color is red change it green otherwise change it to red.
+       button.style.color = 'green';
+    } else {
+       button.style.color = 'red';
+    }
+ }
