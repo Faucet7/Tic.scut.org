293b2aa04a13405c5d8868c01592aed7dde15d12
goodhyz <202230444238@mail.scut.edu.cn>
2023-11-16 21:20:50 +0800

    增加了第二个按钮的随机颜色

--- func.js
+++ func.js
@@ -1,21 +1,18 @@
-function changecolor() {
-    var elem = document.getElementById("num2");
-    var currentColor = window.getComputedStyle(elem).color; // 获取当前颜色
-    var randomColor = getRandomColor(); // 生成一个随机颜色
-    elem.style.color = randomColor;
-  }
-  
+
  function addone() {
     var elem = document.getElementById("num1");
     var present = parseInt(elem.value) || 0;
     elem.value = present + 1;
   }
-
-function getRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
+function randomColor(){
+  this.r = Math.floor(Math.random()*255);
+  this.g = Math.floor(Math.random()*255);
+  this.b = Math.floor(Math.random()*255);
+  this.color = 'rgba('+ this.r +','+ this.g +','+ this.b +',0.8)';
+  return color;
 }
+
+function setBottonRandomColor(){
+  document.getElementById("num2").style.color = randomColor();
+}
+
