12490914c2740ff59828bd8c5a47310e39a18b95
Fauc3t <71Q@Q71.cn>
2023-11-21 14:45:14 +0800

    更新了首页，为下一版本奠基

---
+++ FrontPage/FrontPage.js
@@ -0,0 +1,40 @@
+var pageList = [
+  {
+    version: 1,
+    url: "../FirstVersionPage/index.html",
+  },
+  {
+    version: 2,
+    url: "",
+  },
+  {
+    version: 3,
+    url: "",
+  },
+];
+
+var nowPage = {};
+
+function created() {
+  nowPage = pageList[0];
+}
+created();
+
+function switchPage(step) {
+  switch (nowPage.version + step) {
+    case 0:
+      break;
+    case pageList.length + 1:
+      break;
+    default:
+      nowPage = pageList[nowPage.version + step - 1];
+      break;
+  }
+  document.getElementById("versionBtn").textContent =
+    "Version " + nowPage.version;
+}
+
+function goVersionPage() {
+  window.location.href = nowPage.url;
+  console.log(window.navigator);
+}
